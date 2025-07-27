49f43ddbebabd3c2986dd41b95731ef9d479dd6f
HiroshiIgarashi <tubukiti2008@gmail.com>
2025-07-27 12:20:27 +0900

    絵文字ピッカーのモーダルを作成

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -6,6 +6,7 @@ import { StatusBar } from "expo-status-bar";
 
 import { Button } from "@/components/button";
 import { CircleButton } from "@/components/circle-button";
+import { EmojiPicker } from "@/components/emoji-picker";
 import { IconButton } from "@/components/icon-button";
 import { ImageViewer } from "@/components/image-viewer";
 
@@ -15,13 +16,18 @@ const PlaceholderImage =
 const App: FC = () => {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
   const [showAppOptions, setShowAppOptions] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
 
   const onReset = () => {
     setShowAppOptions(false);
   };
 
   const onAddSticker = () => {
-    //
+    setIsModalVisible(true);
   };
 
   const onSaveImageAsync = async () => {
@@ -49,6 +55,10 @@ const App: FC = () => {
           selectedImage={selectedImage}
         />
       </View>
+      <EmojiPicker
+        isVisible={isModalVisible}
+        onClose={onModalClose}
+      ></EmojiPicker>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
           <View style={styles.optionsRow}>
